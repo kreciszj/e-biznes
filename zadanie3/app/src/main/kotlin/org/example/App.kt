e3fa6453d20f65548f37be58394d20f16962d52d
kreciszj <jakub.krecisz@icloud.com>
2025-04-09 21:24:58 +0200

    feat: complete zadanie3 grade 3.5

--- zadanie3/app/src/main/kotlin/org/example/App.kt
+++ zadanie3/app/src/main/kotlin/org/example/App.kt
@@ -5,6 +5,8 @@ import dev.kord.core.Kord
 import dev.kord.core.behavior.channel.createMessage
 import dev.kord.core.behavior.channel.createEmbed
 import dev.kord.core.entity.channel.MessageChannel
+import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.on
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
@@ -12,16 +14,29 @@ fun main() = runBlocking {
     println("Hello World from Kotlin!")
 
     // Get token
-    val botToken = System.getenv("DISCORD_BOT_TOKEN") 
+    val botToken = System.getenv("DISCORD_BOT_TOKEN")
         ?: throw IllegalArgumentException("No DISCORD_BOT_TOKEN set in environment")
 
     val client = Kord(botToken)
     println("Logged in as: ${client.getSelf().username}")
 
     val channel = client.getChannel(Snowflake(channelId.toULong())) as? MessageChannel
-    
-    channel?.createMessage("Test Message.")
+    channel?.createMessage("Hello World!")
     println("Messages sent to channel $channelId")
 
-    client.shutdown()
+    client.on<MessageCreateEvent> {
+        val isMentioned = message.mentionedUserIds.contains(client.selfId)
+        val content = message.content.trim()
+
+        if (isMentioned) {
+            if (content.contains("!ping", ignoreCase = true)) {
+                message.channel.createMessage("pong!")
+            }
+            else {
+                message.channel.createMessage("Hello! I see you mentioned me, ${message.author?.username}**.")
+            }
+        }
+    }
+
+    client.login()
 }
