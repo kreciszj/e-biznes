26b8b285e3610e184ca5a9cb141d2d4ba38fbc19
kreciszj <jakub.krecisz@icloud.com>
2025-04-16 21:57:29 +0200

    feat: complete zadanie4 grade 3.5

--- zadanie4/main.go
+++ zadanie4/main.go
@@ -6,19 +6,24 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type Product struct {
-	ID          int    `json:"id"`
+	ID          uint   `json:"id" gorm:"primaryKey"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	Price       int    `json:"price"`
 }
 
-var products = []Product{}
-var nextID = 1
+var db *gorm.DB
 
 func listProducts(c echo.Context) error {
+	var products []Product
+	if err := db.Find(&products).Error; err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
 	return c.JSON(http.StatusOK, products)
 }
 
@@ -27,23 +32,22 @@ func getProduct(c echo.Context) error {
 	if err != nil {
 		return c.NoContent(http.StatusBadRequest)
 	}
-	for _, p := range products {
-		if p.ID == id {
-			return c.JSON(http.StatusOK, p)
-		}
+	var product Product
+	if err = db.First(&product, id).Error; err != nil {
+		return c.NoContent(http.StatusNotFound)
 	}
-	return c.NoContent(http.StatusNotFound)
+	return c.JSON(http.StatusOK, product)
 }
 
 func createProduct(c echo.Context) error {
-	var p Product
-	if err := c.Bind(&p); err != nil {
+	var product Product
+	if err := c.Bind(&product); err != nil {
 		return c.NoContent(http.StatusBadRequest)
 	}
-	p.ID = nextID
-	nextID++
-	products = append(products, p)
-	return c.JSON(http.StatusCreated, p)
+	if err := db.Create(&product).Error; err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	return c.JSON(http.StatusCreated, product)
 }
 
 func updateProduct(c echo.Context) error {
@@ -55,14 +59,17 @@ func updateProduct(c echo.Context) error {
 	if err = c.Bind(&body); err != nil {
 		return c.NoContent(http.StatusBadRequest)
 	}
-	for i, p := range products {
-		if p.ID == id {
-			body.ID = id
-			products[i] = body
-			return c.JSON(http.StatusOK, body)
-		}
+	var product Product
+	if err = db.First(&product, id).Error; err != nil {
+		return c.NoContent(http.StatusNotFound)
 	}
-	return c.NoContent(http.StatusNotFound)
+	product.Name        = body.Name
+	product.Description = body.Description
+	product.Price       = body.Price
+	if err = db.Save(&product).Error; err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	return c.JSON(http.StatusOK, product)
 }
 
 func deleteProduct(c echo.Context) error {
@@ -70,16 +77,24 @@ func deleteProduct(c echo.Context) error {
 	if err != nil {
 		return c.NoContent(http.StatusBadRequest)
 	}
-	for i, p := range products {
-		if p.ID == id {
-			products = append(products[:i], products[i+1:]...)
-			return c.NoContent(http.StatusNoContent)
-		}
+	if err = db.Delete(&Product{}, id).Error; err != nil {
+		return c.NoContent(http.StatusInternalServerError)
 	}
-	return c.NoContent(http.StatusNotFound)
+	return c.NoContent(http.StatusNoContent)
 }
 
+
 func main() {
+	var err error
+	db, err = gorm.Open(sqlite.Open("store.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	if err = db.AutoMigrate(&Product{}); err != nil {
+		panic(err)
+	}
+
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
