9ef863bf0a29205492239c64cafc92c2dbc3ff78
kreciszj <jakub.krecisz@icloud.com>
2025-04-16 22:08:02 +0200

    feat: complete zadanie4 grade 4.0

--- zadanie4/main.go
+++ zadanie4/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -15,6 +16,12 @@ type Product struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	Price       int    `json:"price"`
+}
+
+type Cart struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	CreatedAt time.Time `json:"created_at"`
+	Products  []Product `json:"products" gorm:"many2many:cart_products;"`
 }
 
 var db *gorm.DB
@@ -83,6 +90,88 @@ func deleteProduct(c echo.Context) error {
 	return c.NoContent(http.StatusNoContent)
 }
 
+func listCarts(c echo.Context) error {
+	var carts []Cart
+	if err := db.Find(&carts).Error; err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	return c.JSON(http.StatusOK, carts)
+}
+
+func getCart(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+	var cart Cart
+	if err = db.First(&cart, id).Error; err != nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+	return c.JSON(http.StatusOK, cart)
+}
+
+func createCart(c echo.Context) error {
+	var cart Cart
+	if err := db.Create(&cart).Error; err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	return c.JSON(http.StatusCreated, cart)
+}
+
+func listCartProducts(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+	var cart Cart
+	if err = db.Preload("Products").First(&cart, id).Error; err != nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+	return c.JSON(http.StatusOK, cart.Products)
+}
+
+type attachDTO struct {
+	ProductID uint `json:"product_id"`
+}
+
+func addProductToCart(c echo.Context) error {
+	cartID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+	var body attachDTO
+	if err = c.Bind(&body); err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	var cart Cart
+	if err = db.First(&cart, cartID).Error; err != nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+	var product Product
+	if err = db.First(&product, body.ProductID).Error; err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	if err = db.Model(&cart).Association("Products").Append(&product); err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	return c.NoContent(http.StatusCreated)
+}
+
+func removeProductFromCart(c echo.Context) error {
+	cartID, _ := strconv.Atoi(c.Param("id"))
+	prodID, _ := strconv.Atoi(c.Param("pid"))
+
+	var cart Cart
+	if err := db.First(&cart, cartID).Error; err != nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+	if err := db.Model(&cart).Association("Products").Delete(&Product{ID: uint(prodID)}); err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	return c.NoContent(http.StatusNoContent)
+}
 
 func main() {
 	var err error
@@ -91,7 +180,7 @@ func main() {
 		panic(err)
 	}
 
-	if err = db.AutoMigrate(&Product{}); err != nil {
+	if err = db.AutoMigrate(&Product{}, &Cart{}); err != nil {
 		panic(err)
 	}
 
@@ -105,6 +194,15 @@ func main() {
 	g.POST("", createProduct)
 	g.PUT("/:id", updateProduct)
 	g.DELETE("/:id", deleteProduct)
+
+	cg := e.Group("/carts")
+	cg.GET("", listCarts)
+	cg.GET("/:id", getCart)
+	cg.POST("", createCart)
+	
+	cg.GET("/:id/products",  listCartProducts)
+	cg.POST("/:id/products", addProductToCart)
+	cg.DELETE("/:id/products/:pid", removeProductFromCart)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
